05ed389b12a51a01b710dc166e359a01f90e16dc
Sayantan <sayantandatta97@gmail.com>
2018-08-07 10:46:27 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
-Runner.prototype
+Runner.prototype   // to find the abjects 
 
-var defRunner=Runner.prototype.gameOver
+var defRunner=Runner.prototype.gameOver     // to remove all obstacles 
 
-Runner.prototype.gameOver=function(){console.log("Fuck u Chrome")}
+Runner.prototype.gameOver=function(){console.log("Fuck u Chrome")}   // This is fun
 
-Runner.instance_.setSpeed(15000)
+Runner.instance_.setSpeed(15000)     // increeasing the speed of the dino
